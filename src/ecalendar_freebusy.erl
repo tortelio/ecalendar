29d9da8c2509c5121588380742f58b1448cf220c
Bauer Bence <bauerbence5@gmail.com>
2018-08-16 16:53:28 +0200

    Starts freebusy ics response creation TODO: Small fixups and remooving stuffs

---
+++ src/ecalendar_freebusy.erl
@@ -0,0 +1,83 @@
+%%%-------------------------------------------------------------------
+%%% @doc Checks for Free-Busy availability
+%%%-------------------------------------------------------------------
+
+-module(ecalendar_freebusy).
+
+%%====================================================================
+%% Exports
+%%====================================================================
+
+-export([get_availability/0
+         ]).
+
+%%====================================================================
+%% API
+%%====================================================================
+
+%-spec get_availability(binary()) -> binary().
+get_availability() ->
+    {ok, File} = file:read_file("test.ics"),
+    File2 = binary:replace(File, <<"\n">>, <<"\r\n">>, [global]),
+    Original = #{freebusy := #{attendee := Attendee} = FreeBusy} = eics:decode(File2),
+    Username = get_username(Attendee),
+    {StartTime, EndTime} = get_start_end_time(FreeBusy, 3),
+    EventTimes = lists:map(fun(L) -> get_start_end_time(L, 4) end, find_user_events(Username)),
+    create_availability(EventTimes, {StartTime, EndTime}, <<"">>),
+    New = #{data => Original},
+    maps:fold(fun(K,V,A) -> create_ics_body(K,V,A) end, <<"">>, New).
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
+
+find_user_events(User) ->
+    Events = ecalendar_db:get_user_list(User),
+    find_user_events(Events, User, []).
+
+find_user_events([], _, Acc) ->
+    Acc;
+
+find_user_events([Current | Rest], User, Acc) ->
+    {_, [_, _, _, Parsed]} = Current,
+    #{events := Event} = Parsed,
+    find_user_events(Rest, User, lists:merge(Event, Acc)).
+
+get_username(FreebusyData) ->
+    Part = lists:nth(3, lists:nth(4, FreebusyData)),
+    Bin = binary:list_to_bin(Part),
+    lists:nth(1, binary:split(Bin, <<"@">>)).
+
+get_start_end_time(ParsedICS, Place) ->
+    #{dtstart := Start, dtend := End} = ParsedICS,
+    {lists:nth(Place, Start), lists:nth(Place, End)}.
+
+create_availability([], _, Acc) ->
+    Acc;
+
+create_availability([{Start1, End1} | Rest], {Start2, End2}, Acc) ->
+    ToInsert = case (End1 < Start2) or (End2 < Start1) of
+                   false ->
+                       {LStart, LEnd} = {lists:flatten(Start1), lists:flatten(End1)},
+                       create_line(binary:list_to_bin(LStart), binary:list_to_bin(LEnd));
+                   true ->
+                       <<"">>
+               end,
+    create_availability(Rest, {Start2, End2}, <<Acc/binary, ToInsert/binary>>).
+
+create_line(Start, End) ->
+    <<"FREEBUSY;FBTYPE=BUSY:", Start/binary, "Z/", End/binary, "Z\r\n">>.
+
+create_ics_body(Key, Value, Acc) when is_map(Value) ->
+    #{type := Type} = Value,
+    Component = binary:list_to_bin(string:to_upper(atom_to_list(Type))),
+    BeginComp = <<"BEGIN:V", Component/binary, "\r\n">>,
+    FullComp = maps:fold(fun(K,V,A) -> create_ics_body(K,V,A) end, BeginComp, Value),
+    <<Acc/binary, FullComp/binary, "END:V", Component/binary, "\r\n">>;
+
+create_ics_body(Key, Value, Acc) when is_list(Value) ->
+    This = binary:list_to_bin(lists:flatten(Value)),
+    <<Acc/binary, This/binary>>;
+
+create_ics_body(_, _, Acc) ->
+    Acc.
